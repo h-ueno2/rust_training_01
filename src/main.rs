677660432ee36d5aaff10a59a2c367b98a7a9826
h-ueno2 <menbou1212@gmail.com>
2020-04-11 21:26:52 +0900

    4.4. if

--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,20 @@
 fn main() {
-    print_number(5);
-    print_sum(5, 6);
-    println!("{}", add_one(2));
+    let x = 5;
+    if x == 5 {
+        println!("x is 5");
+    } else {
+        println!("x is not 5");
+    }
 
-    let f: fn(i32) -> i32 = add_one;
-    
-    let six = f(5);
-    println!("{}", six);
+    let y = if x == 5 {
+        10
+    } else {
+        15
+    };
+    println!("y is {}", y);
 
-    // 配列
-    let a = [1, 2, 3];
-    let mut m = [1, 2, 3];
-    let a = [0; 20];
-
-    // 配列のスライス
-    let a = [0, 1, 2, 3, 4];
-    let complete = &a[..];
-    let middle = &a[1..4];
-    println!("{}", complete[0]);
-
-    // タプル
-    let mut x = (1, 2);
-    let y = (2, 3);
-    let (x, y, z) = (1, 2, 3);
-    println!("x is {}", x);
-
-    let tuple = (1, 2, 3);
-    let x = tuple.0;
-    println!("x is {}", x);
+    let y = if x == 5 { 10 } else { 15 };
+    println!("y is {}", y);
 }
 
 fn print_number(x: i32) {
