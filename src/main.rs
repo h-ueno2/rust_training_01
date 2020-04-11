fffe586d10b8c0e6aeb28681a06fd87cee86c6cd
h-ueno2 <menbou1212@gmail.com>
2020-04-11 21:20:12 +0900

    4.3. プリミティブ型

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,27 @@ fn main() {
     
     let six = f(5);
     println!("{}", six);
+
+    // 配列
+    let a = [1, 2, 3];
+    let mut m = [1, 2, 3];
+    let a = [0; 20];
+
+    // 配列のスライス
+    let a = [0, 1, 2, 3, 4];
+    let complete = &a[..];
+    let middle = &a[1..4];
+    println!("{}", complete[0]);
+
+    // タプル
+    let mut x = (1, 2);
+    let y = (2, 3);
+    let (x, y, z) = (1, 2, 3);
+    println!("x is {}", x);
+
+    let tuple = (1, 2, 3);
+    let x = tuple.0;
+    println!("x is {}", x);
 }
 
 fn print_number(x: i32) {
