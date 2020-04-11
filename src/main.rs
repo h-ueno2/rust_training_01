02f1fb72e78299e60e4cbfc61a0a08a571fca48f
h-ueno2 <menbou1212@gmail.com>
2020-04-11 20:58:36 +0900

    4.1. 変数束縛

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
 fn main() {
-    println!("Hello, world!");
+    let mut x: i32 = 1;
+    println!("{}", x);
+    x = 7;
+    let x = x;
+    println!("{}", x);
+
+    let y = 4;
+    println!("{}", y);
+    let y = "I can also be bound to text!";
+    println!("{}", y);
 }
